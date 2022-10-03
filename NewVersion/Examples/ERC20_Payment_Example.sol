ca792417e0347c71b55c5f135b12277175bde919
Socarde Paul <84129217+DRIVENpol@users.noreply.github.com>
2022-10-03 15:05:10 +0300

    Update ERC20_Payment_Example.sol

--- NewVersion/Examples/ERC20_Payment_Example.sol
+++ NewVersion/Examples/ERC20_Payment_Example.sol
@@ -3,17 +3,21 @@
 pragma solidity 0.8.10;
 
 // Import the subscription smart contract
-import "https://github.com/DRIVENpol/Subscription-Smart-Contract/blob/main/NewVersion/ETH_payments.sol";
+import "https://github.com/DRIVENpol/Subscription-Smart-Contract/blob/main/NewVersion/ERC20_payment.sol";
 
 
-contract Example_SC is SubscriptionInEth {
+contract Example_SC2 is SubscriptionInErc20 {
 
   uint256 public c = 0;
 
   constructor() {
-    // IMPORTANT!
-    // We set the fee to 1 Ether (1 * 10 * 18)
-    setEthFee(1000000000000000000);
+    // Set the address of the token used for payments
+    // In this case: BUSD (BSC Mainnet)
+    setErc20TokenForPayments(0x55d398326f99059fF775485246999027B3197955);
+    
+    // We set the fee to 1 BUSD (1 * 10 * 18)
+    setErc20Fee(1000000000000000000);
+
   }
 
   // We use the "userPaid" modifier and we allow only users with valid subscriptions to call this function

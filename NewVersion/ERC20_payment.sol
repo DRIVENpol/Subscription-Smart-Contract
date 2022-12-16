9ad1004626d23d9c150fe17ba38685ca1675cff3
Socarde Paul <84129217+DRIVENpol@users.noreply.github.com>
2022-12-16 16:52:56 +0200

    Update ERC20_payment.sol

--- NewVersion/ERC20_payment.sol
+++ NewVersion/ERC20_payment.sol
@@ -1,63 +1,78 @@
 // SPDX-License-Identifier: MIT
 
-// Solidity Version
-pragma solidity ^0.8.0;
-
-// Imports for ERC20 Payments
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+pragma solidity ^0.8.17;
 
 // Ownership
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+interface IToken {
+    function balanceOf(address account) external view returns (uint256);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+}
+
+/**
+ * @title Subscription based smart contract
+ * @notice Pay a monthly subscription erc20 tokens
+ * @author Socarde Paul-Constantin, DRIVENlabs Inc.
+ */
+
 abstract contract SubscriptionInErc20 is Ownable {
 
-    // Who can withdraw the fees
-    address public feeColector;
-    IERC20 public erc20Token;
+    /// @dev IToken Object - IERC20
+    IToken public erc20Token;
 
-    // Struct for payments
+    /// @dev Where the fees will be sent
+    address public feeCollector;
+
+    /// @dev Struct for payments
+    /// @param user Who made the payment
+    /// @param paymentMoment When the last payment was made
+    /// @param paymentExpire When the user needs to pay again
     struct Erc20Payment {
         address user; // Who made the payment
         uint256 paymentMoment; // When the last payment was made
         uint256 paymentExpire; // When the user needs to pay again
     }
 
-    // Array of payments
+    /// @dev Array of erc20 payments
     Erc20Payment[] public erc20Payments;
 
-    // Link an user to its payment
+    /// @dev Link an EOA to an ERC20 payment
     mapping ( address => Erc20Payment ) public userPaymentErc20;
 
-    // Fees
-    uint256 public erc20Fee; // Fee for ethereum payments
+    /// @dev The subscription fee
+    uint256 public erc20Fee;
 
-    // Analytics
+    /// @dev Variables for analytics
     uint256 public totalPaymentsErc20;
     mapping (address => uint256) public userTotalPaymentsErc20;
 
-    // Events
+    /// @dev Events
     event UserPaidErc20(address indexed who, uint256 indexed fee, uint256 indexed period);
 
-    // Constructor
+    /// @dev We transfer the ownership to a given owner
     constructor() {
         _transferOwnership(_msgSender());
-        feeColector = _msgSender();
+        feeCollector = _msgSender();
     }
 
-    // Check if user paid - modifier
+    /// @dev Modifier to check if user's subscription is still active
     modifier userPaid() {
         require(block.timestamp < userPaymentErc20[msg.sender].paymentExpire, "Your subscription expired!"); // Time now < time when last payment expire
         _;
     }
 
+    /// @dev Modifier to check if the caller is an EOA
      modifier callerIsUser() {
         require(tx.origin == msg.sender, "The caller can not be another smart contract!");
         _;
     }
 
-     // Make a payment
-    function paySubscription(uint256 _period) public payable virtual callerIsUser { 
-        require(erc20Token.transfer(address(this), _period * erc20Fee));
+    /// @dev Function to pay the subscription
+    /// @param _period For how many months the user wants to pay the subscription
+    function paySubscription(uint256 _period) external payable virtual callerIsUser { 
+
+        require(erc20Token.transferFrom(msg.sender, address(this), _period * erc20Fee));
 
         totalPaymentsErc20 = totalPaymentsErc20 + msg.value; // Compute total payments in Eth
         userTotalPaymentsErc20[msg.sender] = userTotalPaymentsErc20[msg.sender] + msg.value; // Compute user's total payments in Eth
@@ -67,34 +82,36 @@ abstract contract SubscriptionInErc20 is Ownable {
         userPaymentErc20[msg.sender] = newPayment; // User's last payment
 
         emit UserPaidErc20(msg.sender, erc20Fee * _period, _period);
+
     }
 
-    // Only-owner functions
-    function setErc20Fee(uint256 _newErc20Fee) public virtual onlyOwner {
+    /// @dev Set the monthly Erc20 fee
+    function setErc20Fee(uint256 _newErc20Fee) external virtual onlyOwner {
         erc20Fee = _newErc20Fee;
     }
 
-    function setErc20TokenForPayments(address _newErc20Token) public virtual onlyOwner {
-        erc20Token = IERC20(_newErc20Token);
+    /// @dev Set the erc20 token for payments
+    function setErc20TokenForPayments(address _newErc20Token) external virtual onlyOwner {
+        erc20Token = IToken(_newErc20Token);
     }
 
-    function setNewPaymentCollector(address _feeColector) public virtual onlyOwner {
-        feeColector = _feeColector;
+    /// @dev Set a new payment collector
+    function setNewPaymentCollector(address _feeCollector) public virtual onlyOwner {
+        feeCollector = _feeCollector;
     }
 
-    function withdrawErc20() public virtual onlyOwner {
-         erc20Token.transfer(feeColector, erc20Token.balanceOf(address(this)));
+    /// @dev Withdraw erc20 tokens
+    function withdrawErc20() external virtual onlyOwner {
+        require(erc20Token.transferFrom(address(this), feeCollector, erc20Token.balanceOf(address(this))), "ERC20 Transfer Failed!");
     }
 
-    // Getter
-    function lastPaymentOfUser(address _user) public view virtual returns(uint256) {
+    /// @dev Get the last payment of user in Erc20
+    function lastPaymentOfUser(address _user) external view virtual returns(uint256) {
         return userPaymentErc20[_user].paymentMoment;
     }
 
-    function paymentsInSmartContract() public view virtual returns(uint256) {
+    /// @dev Get the smart contract's balance of tokens
+    function paymentsInSmartContract() external view virtual returns(uint256) {
         return erc20Token.balanceOf(address(this));
-    }
-    
-    // Created by @polthedev (https://twitter.com/polthedev) | DRIVENlabs Inc. (www.drivenecosystem.com)
-      
+    } 
 }
